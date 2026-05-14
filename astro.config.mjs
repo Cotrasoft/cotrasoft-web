26ce4f8001bfd503b70eb0558266c07ec9c4b6b1
Nathan Redblur <jon.nathan.rich@gmail.com>
2026-05-13 22:28:29 -0500

    cloudflare config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://cotrasoft.co',
+	output: "static",
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
